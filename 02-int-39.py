7000303770ebab3d445c0eb9c06d162ec0b8f76e
Marat Ramazanov <sadzax@gmail.com>
2022-11-09 18:53:33 +0300

    v.0.0.2.

--- 02-int-39.py
+++ 02-int-39.py
@@ -5,4 +5,19 @@
 
 import sadzax
 
-a = sadzax.input_int()
+a = sadzax.input_int("enter a: ","Only ints!",1,None,None)
+b = sadzax.input_int("enter b: ","Only ints!",1,None,None)
+
+while True:    
+    c = sadzax.input_int("enter c: ","Only ints!",1,None,None)
+    if c > a or c > b:
+        print(f'"c" must be under "a" and "b"')
+        continue
+    else:
+        break
+
+Q = (a // c) * (b // c)
+P = (a * b) - (Q * c * c)
+
+print(Q)
+print(P)
