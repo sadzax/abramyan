ed9b2a43ac43a489214314f6f8fc2cccf119f89d
Marat Ramazanov <sadzax@gmail.com>
2022-11-10 11:05:54 +0300

    v.0.0.4.

--- 01-begin-39.py
+++ 01-begin-39.py
@@ -1,12 +1,26 @@
 # Begin39
-# Найти корни квадратного уравнения A·x2 + B·x + C = 0, заданного своими коэффициентами A, B, C (коэффициент A не равен 0), если известно, 
-# что дискриминант уравнения положителен. Вывести внача-ле меньший, а затем больший из найденных корней.
+# Найти корни квадратного уравнения A·x2 + B·x + C = 0, 
+# заданного своими коэффициентами A, B, C (коэффициент A не равен 0), 
+# если известно, что дискриминант уравнения положителен. 
+# Вывести внача-ле меньший, а затем больший из найденных корней.
 
 import sadzax
+import math
 
-error = "Please enter only floats"
-side_a = sadzax.input_float(f"Enter Side A: ",error,None,None,0)
-side_b = sadzax.input_float(f"Enter Side B: ",error,None,None,0)
-side_c = sadzax.input_float(f"Enter Side C: ",error,None,None,0)
+error = "Please enter only digits"
+while True:
+    a = sadzax.input_float(f"Enter A: ",error,None,None,0)
+    b = sadzax.input_float(f"Enter B: ",error,None,None,None)
+    c = sadzax.input_float(f"Enter C: ",error,None,None,None)
+    disc = (b ** 2) - (4 * a * c)
+    if disc < 0:
+        print('discriminant must be positive, try again')
+        continue
+    if disc >= 0:
+        break
 
-disc = 
+x1 = ((b * -1) - math.sqrt(disc)) / (2 * a)
+x2 = ((b * -1) + math.sqrt(disc)) / (2 * a)
+answers = sorted([x1, x2])
+
+print(answers)
--- sadzax.py
+++ sadzax.py
@@ -19,7 +19,18 @@ def arg_isnt_f(i, arg):
     else:
         return True
 
-def inputter_int(input_descripton, arg_error, arg_min, arg_max, arg_isnt):
+def arg_isnt_in_list_f(i, arg):
+    if arg is not None:
+        if isinstance(arg, (list, tuple)):
+            for el in arg:
+                if el == i:
+                    return False
+        elif i == arg or arg is True:
+            return False
+    else:
+        return True
+ 
+def input_int(input_descripton, arg_error, arg_min, arg_max, arg_isnt):
     while True:
         try:
             i = int(input(input_descripton))
@@ -31,11 +42,11 @@ def inputter_int(input_descripton, arg_error, arg_min, arg_max, arg_isnt):
             print(arg_error)
             continue
 
-def inputter_float(input_descripton, arg_error, arg_min, arg_max, arg_isnt):
+def input_float(input_descripton, arg_error, arg_min, arg_max, arg_isnt_in_list):
     while True:
         try:
             i = float(input(input_descripton))
-            if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_f(i, arg_isnt) is False:
+            if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_in_list_f(i, arg_isnt_in_list) is False:
                 print(arg_error)
                 continue
             return i
@@ -43,17 +54,14 @@ def inputter_float(input_descripton, arg_error, arg_min, arg_max, arg_isnt):
             print(arg_error)
             continue
 
-def inputter_float(input_descripton, arg_error, arg_exclude_list):
+def input_str(input_descripton, arg_error, arg_min, arg_max, arg_isnt_in_list):
     while True:
         try:
-            i = float(input(input_descripton))
-            if type(arg
-            for i in arg_exclude_list:
-            if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_f(i, arg_isnt) is False:
+            i = str(input(input_descripton))
+            if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_in_list_f(i, arg_isnt_in_list) is False:
                 print(arg_error)
                 continue
             return i
         except:
             print(arg_error)
             continue
-
