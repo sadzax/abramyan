fe65b1a44c513c80303b8067dcd56156d2b9cd47
Marat Ramazanov <sadzax@gmail.com>
2022-11-10 11:10:38 +0300

    done

--- 03-boolean-22.py
+++ 03-boolean-22.py
@@ -1,9 +1,20 @@
 # Дано трехзначное число. Проверить истинность высказывания:
 # «Цифры данного числа образуют возрастающую или убывающую после-
 # довательность».
+# ЗАДАНИЕ ПЕРЕРАБОТАНО: дано целое число
 
 import sadzax
-num = sadzax.input_int("a = ","error",1,None,None)
+# num = sadzax.input_int("a = ","error",1,None,None)
+
+def look_for_orders_in_number(dgt, counter):
+    dgt = dgt // 10
+    if dgt < 0:
+        return counter
+    else:
+        return counter
+        counter += 1
+        look_for_orders_in_number(dgt//10, counter)
+    
 
 def check(i):
     if (i // 10) < 10:
@@ -12,4 +23,4 @@ def check(i):
     else:
         return False
 
-print(check(num))
+print(look_for_orders_in_number(5000, 0))
--- sadzax.py
+++ sadzax.py
@@ -1,67 +1,76 @@
-def arg_min_f(i, arg):
-    if arg is not None:
-        if i < arg:
-            return False
-    else:
-        return True
+class Enter(object):
+    def arg_min_f(i, arg):
+        if arg is not None:
+            if i < arg:
+                return False
+        else:
+            return True
 
-def arg_max_f(i, arg):
-    if arg is not None:
-        if i > arg:
-            return False
-    else:
-        return True
+    def arg_max_f(i, arg):
+        if arg is not None:
+            if i > arg:
+                return False
+        else:
+            return True
 
-def arg_isnt_f(i, arg):
-    if arg is not None:
-        if i == arg or arg is True:
-            return False
-    else:
-        return True
+    def arg_isnt_f(i, arg):
+        if arg is not None:
+            if i == arg or arg is True:
+                return False
+        else:
+            return True
 
-def arg_isnt_in_list_f(i, arg):
-    if arg is not None:
-        if isinstance(arg, (list, tuple)):
-            for el in arg:
-                if el == i:
-                    return False
-        elif i == arg or arg is True:
-            return False
-    else:
-        return True
- 
-def input_int(input_descripton, arg_error, arg_min, arg_max, arg_isnt):
-    while True:
-        try:
-            i = int(input(input_descripton))
-            if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_f(i, arg_isnt) is False:
+    def arg_isnt_in_list_f(i, arg):
+        if arg is not None:
+            if isinstance(arg, (list, tuple)):
+                for el in arg:
+                    if el == i:
+                        return False
+            elif i == arg or arg is True:
+                return False
+        else:
+            return True
+    
+    def input_int(input_descripton, arg_error, arg_min, arg_max, arg_isnt):
+        while True:
+            try:
+                i = int(input(input_descripton))
+                if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_f(i, arg_isnt) is False:
+                    print(arg_error)
+                    continue
+                return i
+            except:
                 print(arg_error)
                 continue
-            return i
-        except:
-            print(arg_error)
-            continue
 
-def input_float(input_descripton, arg_error, arg_min, arg_max, arg_isnt_in_list):
-    while True:
-        try:
-            i = float(input(input_descripton))
-            if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_in_list_f(i, arg_isnt_in_list) is False:
+    def input_float(input_descripton, arg_error, arg_min, arg_max, arg_isnt_in_list):
+        while True:
+            try:
+                i = float(input(input_descripton))
+                if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_in_list_f(i, arg_isnt_in_list) is False:
+                    print(arg_error)
+                    continue
+                return i
+            except:
                 print(arg_error)
                 continue
-            return i
-        except:
-            print(arg_error)
-            continue
 
-def input_str(input_descripton, arg_error, arg_min, arg_max, arg_isnt_in_list):
-    while True:
-        try:
-            i = str(input(input_descripton))
-            if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_in_list_f(i, arg_isnt_in_list) is False:
+    def input_str(input_descripton, arg_error, arg_min, arg_max, arg_isnt_in_list):
+        while True:
+            try:
+                i = str(input(input_descripton))
+                if arg_min_f(i, arg_min) is False or arg_max_f(i, arg_max) is False or arg_isnt_in_list_f(i, arg_isnt_in_list) is False:
+                    print(arg_error)
+                    continue
+                return i
+            except:
                 print(arg_error)
                 continue
-            return i
-        except:
-            print(arg_error)
-            continue
+
+def find_number_of_digits(dgt, counter):
+    dgt = abs(dgt)
+    if dgt <= 0:
+        print(counter)
+        return
+    else:
+        find_number_of_digits(dgt//10, counter + 1)
